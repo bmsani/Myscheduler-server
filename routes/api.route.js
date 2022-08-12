1e11385df66cf1159421aee05e3945890e7e1997
bmsani <bmsani6@gmail.com>
2022-08-12 10:02:07 +0600

    event refress token code updated

--- routes/api.route.js
+++ routes/api.route.js
@@ -5,6 +5,8 @@ const GOOGLE_CLIENT_ID =
   "246190552758-iv4qnbua1chul41b87mfch0gsoeqe8bj.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
 
+const REFRESH_TOKEN = "1//0giltSolzyouKCgYIARAAGBASNwF-L9IrGD-Bl5LuIk0h49KDzLEjR7DwJJ7fmq0uaS8GckYKgUxFnXhDb9h_YFX3tYBYhluiIIk"
+
 const oauth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
@@ -28,7 +30,7 @@ router.post("/create-tokens", async (req, res, next) => {
 router.post("/create-event", async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    const refreshToken = authHeader.split(" ")[1];
+    const refreshToken = REFRESH_TOKEN;
     const { bookingConfirm } = req.body;
     const { summary, description, email, startTime, endTime } = bookingConfirm;
     console.log(summary, description, email, startTime, endTime);
@@ -38,6 +40,8 @@ router.post("/create-event", async (req, res, next) => {
     const response = await calendar.events.insert({
       auth: oauth2Client,
       calendarId: "primary",
+      conferenceDataVersion: 1,
+      sendNotifications: true,
       requestBody: {
         summary: summary,
         description: description,
@@ -49,6 +53,12 @@ router.post("/create-event", async (req, res, next) => {
           dateTime: endTime,
         },
         attendees: [{ email: email }],
+        conferenceData: {
+          createRequest: {
+              requestId: "sample123",
+              conferenceSolutionKey: { type: "hangoutsMeet" },
+          },
+      },
       },
     });
     res.send(response);
