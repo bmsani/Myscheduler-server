ea36ce584fbab2e063d962fc065b7a78c8276107
ArafatSadi1 <arafatsadi1@gmail.com>
2022-08-23 14:04:53 +0600

    update all api

--- routes/api.route.js
+++ routes/api.route.js
@@ -19,16 +19,43 @@ const oauth2Client = new google.auth.OAuth2(
   "http://localhost:3000"
 );
 
-async function run() {
-  router.get("/", async (req, res, next) => {
-    res.send({ message: "Ok api is working 🚀" });
+const verifyJWT = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Access forbidden" });
+    }
+    req.decoded = decoded;
+    next();
   });
+};
 
+async function run() {
   await client.connect();
   const usersCollection = client.db("MyScheduler").collection("users");
   const bookingConfirmCollection = client
     .db("MyScheduler")
     .collection("bookingConfirm");
+
+  const verifyAdmin = async (req, res, next) => {
+    const requester = req.decoded.email;
+    const requesterAccount = await usersCollection.findOne({
+      email: requester,
+    });
+    if (requesterAccount.role === "admin") {
+      next();
+    } else {
+      res.status(403).send({ message: "forbidden access" });
+    }
+  };
+
+  router.get("/", async (req, res, next) => {
+    res.send({ message: "Ok api is working 🚀" });
+  });
 
   router.post("/create-tokens", async (req, res, next) => {
     try {
@@ -113,21 +140,21 @@ async function run() {
   });
 
   // get user all booked events
-  router.get("/bookedEvents/:email", async (req, res) => {
+  router.get("/bookedEvents/:email", verifyJWT, async (req, res) => {
     const email = req.params.email;
     const filter = { hostEmail: email };
     const result = await bookingConfirmCollection.find(filter).toArray();
     res.send(result);
   });
   // delete single booked events
-  router.delete("/bookedEventDelete/:id", async (req, res) => {
+  router.delete("/bookedEventDelete/:id", verifyJWT, async (req, res) => {
     const id = req.params.id;
     const filter = { _id: ObjectId(id) };
     const result = await bookingConfirmCollection.deleteOne(filter);
     res.send(result);
   });
   // get all booked events
-  router.get("/allBookedEvents", async (req, res) => {
+  router.get("/allBookedEvents", verifyJWT, verifyAdmin, async (req, res) => {
     const result = await bookingConfirmCollection.find({}).toArray();
     res.send(result);
   });
